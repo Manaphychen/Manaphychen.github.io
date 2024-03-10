7a5702757d4aad1ad8c27e9cf2f0e2efcddfe8ca
manaphy <535395726@qq.com>
2024-03-10 16:04:31 +0800

    处理图片问题

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -43,11 +43,12 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         ],
       },
       {
-        text: '页面',
-        link: '/ui/',
+        text: '数据库',
+        link: '/db/',
         items: [
-          {text: 'HTML', link: '/pages/8309a5b876fc95e3/'},
-          {text: 'CSS', link: '/pages/0a83b083bdf257cb/'},
+          {text: 'MySQL', link: '/mysql/base/'},
+          {text: 'Redis', link: '/redis/base/'},
+          {text: 'MongoDB', link: '/mongodb/index/'},
         ],
       },
       {
@@ -308,26 +309,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
         options: {
           bgColor: 'rgba(0,0,0,0.6)',
-        },
-      },
-    ],
-    [
-      'vuepress-plugin-comment', // 评论
-      {
-        choosen: 'gitalk',
-        options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
-          // distractionFreeMode: true,
-          pagerDirection: 'last', // 'first'正序 | 'last'倒序
-          id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-          title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-          labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-          body:
-            '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
         },
       },
     ],
